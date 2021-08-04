04cddc777ea81e5aec91865fdb6d091cf79bb26b
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2021-08-05 00:58:56 +0800

    added more bg

--- main.js
+++ main.js
@@ -1,54 +1,17 @@
-//let selectedPage = 1;
-
 document.addEventListener('DOMContentLoaded', function() {
-  /*window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll',window.pageYOffset / (document.querySelector("#page1").offsetHeight - window.innerHeight));
-  }, false);*/
-  /*
-  let landingPage = document.getElementById("page1");
-  $("#page1").addClass("fadein");
-  landingPage.addEventListener('animationend', () => {
-    $("#page1").removeClass("fadein");
-    landingPage.style.opacity = 1;
-  });*/
-	/*document.querySelectorAll('.navigation-button').forEach((button) => {
-    button.addEventListener('click', NavigateTo(button.dataset.pageid));
-  });*/
-	document.getElementById("site-bg").style.top = "100vh";
-  NavigateTo(1);
+  /*NavigateTo(1);
   $('#page1').addClass("fadein");
     document.querySelector('#page1').addEventListener('animationend', () => {
       $('#page1').removeClass("fadein");
       document.querySelector('#page1').style.opacity = 1;
-    });
+    });*/
 })
 
 function NavigateTo(pageId) {
 	let page = document.querySelector("#page"+pageId);
-  //console.log(page);
   window.scrollTo({
     top: page.offsetTop,
     behavior: 'smooth'
-  });
-}
-
-function ChangeActivePage(pageId) {
-  let prevPage = document.querySelector("#page"+selectedPage);
-  $("#page"+selectedPage).addClass("fadeout");
-  prevPage.addEventListener('animationend', () => {
-    $("#page"+selectedPage).removeClass("fadeout");
-    prevPage.style.opacity = 0;
-    //console.log(selectedPage);
-  });
-
-  selectedPage = pageId;
-
-  $("#page"+pageId).addClass("fadein");
-  let newPage = document.querySelector("#page"+pageId);
-  newPage.addEventListener('animationend', () => {
-    $("#page"+pageId).removeClass("fadein");
-    newPage.style.opacity = 1;
-    //console.log(newPage);
   });
 }
 
@@ -56,7 +19,6 @@ function ChangeActivePage(pageId) {
 function callback (fracs, prev_fracs) {
   //console.log(this);
   if (fracs.visible > 0.5 && this.style.opacity == 0) {
-    //ChangeActivePage($(this).prop('id').slice(-1));
     $(this).addClass("fadein");
     this.addEventListener('animationend', () => {
       $(this).removeClass("fadein");
@@ -72,10 +34,13 @@ function callback (fracs, prev_fracs) {
   }
 }
 
+function landingCallback (fracs, prev_fracs) {
+
+}
+
 // checks how much of each queried div is visible
 $(window).scroll(function () {
-  $(".fadeable").fracs(callback);
-  //$("#page1").fracs(callback);
+  let fracs = $("#page1").fracs(callback);
   //$(".wrap").fracs(callback);
   //console.log($(".page"))
   //$(".content-block").fracs(callback);
@@ -86,30 +51,3 @@ $(window).scroll(function () {
 function setTranslate(xPos, yPos, el) {
   el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
 }
-/*
-// parallax scrolling plugin
-  $.fn.parallax = function(options){
-    var $$ = $(this);
-    offset = $$.offset();
-    var defaults = {
-      'start': 0,
-      'stop': offset.top + $$.height(),
-      'coeff': 0.95
-    };
-    var opts = $.extend(defaults, options);
-    return this.each(function(){
-      $(window).bind('scroll', function() {
-        windowTop = $(window).scrollTop();
-        if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
-          newCoord = windowTop * opts.coeff;
-          $$.css({
-              'background-position': '0 '+ newCoord + 'px'
-          });
-        }
-      });
-    });
-  }
-*/
-// call the plugin
-// $('.bg-content').parallax({ 'coeff':-0.65 });
-// $('.section .inner').parallax({ 'coeff':1.15 });
--- parallax.js
+++ parallax.js
@@ -14,10 +14,12 @@ function scrollLoop() {
   // We only change the Y variable
   setTranslate(0, yScrollPosition * -0.15, document.querySelector("#landing-bg"));
   setTranslate(0, yScrollPosition * -0.05, document.querySelector("#title-text"));
-  setTranslate(0, yScrollPosition * -0.25, document.querySelector("#site-bg"));
+  //setTranslate(0, yScrollPosition * -0.25, document.querySelector("#site-bg"));
   setTranslate(0, yScrollPosition * document.querySelector("#c1").dataset.parallax, document.querySelector("#c1"));
   setTranslate(0, yScrollPosition * document.querySelector("#c2").dataset.parallax, document.querySelector("#c2"));
   setTranslate(0, yScrollPosition * document.querySelector("#c3").dataset.parallax, document.querySelector("#c3"));
+  setTranslate(0, yScrollPosition * document.querySelector("#lines-bg").dataset.parallax, document.querySelector("#lines-bg"));
+  setTranslate(0, yScrollPosition * document.querySelector("#lines-bg2").dataset.parallax, document.querySelector("#lines-bg2"));
   // We use requestAnimationFrame to target the GPU instead of the CPU
   requestAnimationFrame(scrollLoop);
 }
